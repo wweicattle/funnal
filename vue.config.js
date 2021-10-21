d202d0c9bafd04cf419c58253fae83fb0e96dbd2
wwcattle <wwcattle@qq.com>
2021-10-21 11:38:58 +0800

    edit global attr for devserver

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,33 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-    lintOnSave: false,
-    productionSourceMap:false,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  lintOnSave: false,
+  productionSourceMap: false,
+  devServer: {
     disableHostCheck: true,
-    devServer: {
-      // hot:true,
-      proxy: {
-        '/dev': {
-          target: 'http://192.168.35.96:8900/svr-become',
-          pathRewrite: {
-            '^/dev': ''
-          }
-        },
-        "/upload":{
-          target: 'http://192.168.35.131:9501/fileOss/base642Oss',
-          pathRewrite: {
-            '^/upload': ''
-          }
+    proxy: {
+      '/dev': {
+        target: 'http://192.168.35.96:8900/svr-become',
+        pathRewrite: {
+          '^/dev': ''
         }
-      }
-    },
-    configureWebpack: {
-      resolve: {
-        alias: {
-          "assets": "@/assets",
-          "network": "@/network",
-          "components": "@/components",
-          "views": "@/views",
-          "utils": "@/utils",
+      },
+      "/upload": {
+        target: 'http://192.168.35.131:9501/fileOss/base642Oss',
+        pathRewrite: {
+          '^/upload': ''
         }
       }
     }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "assets": "@/assets",
+        "network": "@/network",
+        "components": "@/components",
+        "views": "@/views",
+        "utils": "@/utils",
+      }
+    }
   }
-  
+}
