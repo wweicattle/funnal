daeeb1902453034b5bd7b8c9b0efe0f3be4a8c0b
ruruli <kbd_nssb@163.com>
2021-10-25 08:40:52 +0800

    样式小调整

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,7 @@ import YrTwo from './components/BatchDialogs/YrTwo.vue';
 import YrThree from './views/StorePolicy/TestDialog/YrThree.vue';
 import YrFour from './views/StorePolicy/TestDialog/YrFour.vue';
 import YrFive from './views/StorePolicy/TestDialog/YrFive.vue';
-import YrSix from './components/BatchDialogs/pop9.vue';
+import YrSix from './views/StorePolicy/TestDialog/YrSix.vue';
 
 export default {
   data() {
--- src/views/StorePolicy/TestDialog/YrFive.vue
+++ src/views/StorePolicy/TestDialog/YrFive.vue
@@ -43,10 +43,10 @@
           <!-- 行 -->
           <div class="b-bable-content-hr" style="height:200px">
             <div>
-              <el-input class="my-line-input" v-model="setForm.area"></el-input>
+              <el-input class="my-line-input" oninput="value=value.replace(/[^0-9.]/g,'')" v-model="setForm.area"></el-input>
             </div>
             <div>
-              <el-input class="my-line-input" v-model="setForm.rent"></el-input><span>万元</span>
+              <el-input class="my-line-input" type="number" v-model="setForm.rent"></el-input><span>万元</span>
             </div>
             <div>
               <div class="val">
@@ -59,10 +59,10 @@
             </div>
             <div class="justifys">
               <el-input class="my-line-input" v-model="setForm.area"></el-input><span>m²</span>
-              <span> * </span>
-              <el-input class="my-line-input"></el-input><span> 万元/m²</span>
-              <span> = </span>
-              <el-input class="my-line-input"></el-input><span> 万元</span>
+              <span style="padding:0 5px;"> x </span>
+              <el-input class="my-line-input"></el-input><span>万元/m²</span>
+              <span style="padding:0 5px;"> = </span>
+              <el-input class="my-line-input"></el-input><span>万元</span>
             </div>
             <div>
               <div class="val">
@@ -79,7 +79,7 @@
         <div class="basic-c radioL">
           <span class="tit" style="width:170px;">是否要求改图（非必填）</span>
           <div class="val">
-            <el-radio-group>
+            <el-radio-group v-model="setForm.level">
               <el-radio label="0">贸易公司要求改图</el-radio>
               <el-radio label="1">总公司要求改图</el-radio>
             </el-radio-group>
@@ -88,17 +88,15 @@
         <div class="basic-c large">
           <span class="tit">改图次数</span>
           <div class="val">
-            <!-- <el-select placeholder="请选择"> -->
-            <!-- <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option> -->
-            <!-- <el-option value="11"></el-option> -->
-            <!-- </el-select> -->
-            <el-input class="value"></el-input>
+            <el-select placeholder="请选择" v-model="setForm.numSelect">
+              <el-option v-for="(num,index) in setForm.num" :key="index" :label="num" :value="num"></el-option>
+            </el-select>
           </div>
         </div>
         <div class="basic-c large">
           <span class="tit">改图原因</span>
           <div class="val">
-            <el-input></el-input>
+            <el-input v-model="setForm.reason"></el-input>
           </div>
         </div>
       </div>
@@ -157,7 +155,11 @@ export default {
       nodeResult: {},
       setForm: {
         area: '',
-        rent: ''
+        rent: '',
+        level: '0',
+        num: [1, 2, 3],
+        numSelect: '',
+        reason: ''
       }
     };
   },
@@ -199,16 +201,25 @@ export default {
     .b-bable-content-hr {
       > div {
         &:first-child {
-          width: 65px;
+          width: 85px;
+          .my-line-input {
+            width: 50px;
+          }
         }
         &:nth-child(2) {
-          width: 80px;
+          width: 85px;
+          .my-line-input {
+            width: 45px;
+          }
         }
         &:nth-child(3) {
           width: 120px;
         }
         &:nth-child(4) {
-          width: 150px;
+          width: 180px;
+          .my-line-input {
+            width: 40px;
+          }
         }
       }
     }
