aa377fff94bd3c667bd0d6052d9352c853791822
wuwei <996196542@qq.com>
2022-04-23 16:58:19 +0800

    test one

--- src/App.vue
+++ src/App.vue
@@ -491,6 +491,7 @@ export default {
   },
   created() {
     console.log("this is testgitsss");
+    console.log(" this is testone");
     // 动态加载是不能保证一定加载完成，所以加个异步保证一定能够返回的是有值的
     setTimeout((val) => {
       this.mapComponents = mapComponents;
