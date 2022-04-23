dd23c7a56a5da960addd301489e8fd157a759517
wuwei <996196542@qq.com>
2022-04-23 19:44:45 +0800

    dev5

--- src/App.vue
+++ src/App.vue
@@ -493,6 +493,9 @@ export default {
 
 console.log("master1");
     console.log("this is develop");
+    console.log("this is develop5");
+    console.log("this is develop4");
+
     console.log("this is develop11");
     console.log("this is testone 3");
     console.log("master!");
